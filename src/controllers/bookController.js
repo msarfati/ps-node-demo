dee30275fe7c8062e424b67de51a593b478ecc62
Michael Sarfati <michael.sarfati@utoronto.ca>
2018-11-25 22:38:40 -0500

    bookController

---
+++ src/controllers/bookController.js
@@ -0,0 +1,58 @@
+const db = require('../../db');
+// const debug = require('debug')('app:bookController');
+
+function bookController(nav) {
+  function middleware(req, res, next) {
+    // if (req.user) {
+    //   next();
+    // } else {
+    //   res.redirect('/');
+    // }
+    next();
+  }
+
+
+  function getIndex(req, res) {
+    (async function query() {
+      const books = await db.pgPool.query(db.queries.getBooks);
+
+      res.render(
+        'bookListView',
+        {
+          nav,
+          books: books.rows,
+          title: 'Library'
+        }
+      );
+    }());
+  }
+
+  function renderByID(req, res) {
+    res.render(
+      'bookView',
+      {
+        nav,
+        book: req.result,
+        title: 'Library'
+      }
+    );
+  }
+
+  function getByID(req, res, next) {
+    (async function query() {
+      const { id } = req.params;
+      const result = await db.pgPool.query(db.queries.getBookByID, [id]);
+      [req.result] = result.rows;
+      next();
+    }());
+  }
+
+  return {
+    middleware,
+    getIndex,
+    renderByID,
+    getByID,
+  };
+}
+
+module.exports = bookController;
--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,54 +1,22 @@
-const debug = require('debug')('app:bookRoutes');
 const express = require('express');
-const db = require('../../db');
+const bookController = require('../controllers/bookController');
 
 const bookRouter = express.Router();
 
 function router(nav) {
-  bookRouter.use((req, res, next) => {
-    if (req.user) {
-      next();
-    } else {
-      res.redirect('/');
-    }
-  });
+  const {
+    middleware, getIndex, getByID, renderByID
+  } = bookController(nav);
+
+  bookRouter.use(middleware);
 
   bookRouter.route('/')
-    .get((req, res) => {
-      (async function query() {
-        const result = await db.pgPool.query(db.queries.getBooks);
-
-        res.render(
-          'bookListView',
-          {
-            nav,
-            books: result.rows,
-            title: 'Library'
-          }
-        );
-      }());
-    });
+    .get(getIndex);
 
 
   bookRouter.route('/:id')
-    .all((req, res, next) => {
-      (async function query() {
-        const { id } = req.params;
-        const result = await db.pgPool.query(db.queries.getBookByID, [id]);
-        [req.result] = result.rows;
-        next();
-      }());
-    })
-    .get((req, res) => {
-      res.render(
-        'bookView',
-        {
-          nav,
-          book: req.result,
-          title: 'Library'
-        }
-      );
-    });
+    .all(getByID)
+    .get(renderByID);
 
   return bookRouter;
 }
